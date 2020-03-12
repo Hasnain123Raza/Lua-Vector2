0d87554ef3d0924c6332f1e9e142c5b8e25198db
Hasnain Raza <49315454+Hasnain123Raza@users.noreply.github.com>
2020-03-12 10:55:49 -0400

    Update Vector2.lua

--- Vector2.lua
+++ Vector2.lua
@@ -68,25 +68,81 @@
 
 local MODULE = {}
 
---// CONSTANTS //--
+--// METAMETHODS //--
 
+local function umn(self)
+	return (self * -1)
+end
 
+local function add(self, value)
+	if (type(value) == "number") then
+		return MODULE.addScalar(self, value)
+	elseif (type(value) == "table") then
+		if (value.className) and (value.className == "Vector2") then
+			return MODULE.addVectors(self, value)
+		else
+			error("attempt to perform arithmetic (add) on " .. self.className .. " and unknown")
+		end
+	else
+		error("attempt to perform arithmetic (add) on " .. self.className .. " and " .. type(value))
+	end
+end
 
---// VARIABLES //--
+local function sub(self, value)
+	if (type(value) == "number") then
+		return MODULE.subtractScalar(self, value)
+	elseif (type(value) == "table") then
+		if (value.className) and (value.className == "Vector2") then
+			return MODULE.subtractVectors(self, value)
+		else
+			error("attempt to perform arithmetic (subtract) on " .. self.className .. " and unknown")
+		end
+	else
+		error("attempt to perform arithmetic (subtract) on " .. self.className .. " and " .. type(value))
+	end
+end
 
+local function mul(self, value)
+	if (type(value) == "number") then
+		return MODULE.multiplyScalar(self, value)
+	elseif (type(value) == "table") then
+		if (value.className) and (value.className == "Vector2") then
+			return MODULE.multiplyVectors(self, value)
+		else
+			error("attempt to perform arithmetic (multiply) on " .. self.className .. " and unknown")
+		end
+	else
+		error("attempt to perform arithmetic (multiply) on " .. self.className .. " and " .. type(value))
+	end
+end
 
+local function div(self, value)
+	if (type(value) == "number") then
+		return MODULE.divideScalar(self, value)
+	elseif (type(value) == "table") then
+		if (value.className) and (value.className == "Vector2") then
+			return MODULE.divideVectors(self, value)
+		else
+			error("attempt to perform arithmetic (divide) on " .. self.className .. " and unknown")
+		end
+	else
+		error("attempt to perform arithmetic (divide) on " .. self.className .. " and " .. type(value))
+	end
+end
+
+local function tostringMetamethod(self)
+	return ("( " .. self.x .. ", " .. self.y .. ")")
+end
+
+local function eq(self, value)
+	return MODULE.equals(self, value, 0.01)
+end
 
 --// CONSTRUCTOR //--
 
 function MODULE.new(x, y)
-	
-	--[[
-		
-		Creates a new Vector2 object.
-		
-	--]]
-	
-	-- Handle Input --
+
+	-- Handles Input --
 	if (x) and (type(x) ~= "number") then error("number expected, got " .. type(x)) end
 	if (y) and (type(y) ~= "number") then error("number expected, got " .. type(y)) end
 	----
@@ -106,13 +162,6 @@ function MODULE.new(x, y)
 		},
 		{
 			__index = function(self, index)
-					
-				--[[
-					
-					Appropriately handles indexes to Vector2 objects.
-					
-				--]]
-				
 				if (index == "magnitude") then
 					return MODULE.getMagnitude(self)
 				elseif (index == "unit") then
@@ -120,17 +169,9 @@ function MODULE.new(x, y)
 				else
 					return (dataTable[index])
 				end
-				
 			end,
 			
 			__newindex = function(self, index, newValue)
-									
-				--[[
-					
-					Appropriately handles editing properties of Vector2 objects.
-					
-				--]]
-				
 				if (index == "x") or (index == "y") then
 					if (newValue) and (type(newValue) ~= "number") then
 						error("number expected, got " .. type(index))
@@ -140,132 +181,15 @@ function MODULE.new(x, y)
 				else
 					error(newValue .. " cannot be assigned")
 				end
-				
 			end,
 			
-			__unm = function(self)
-									
-				--[[
-					
-					Appropriately handles unary operator and returns the result.
-					
-				--]]
-				
-				return (self * -1)
-				
-			end,
-			
-			__add = function(self, value)
-									
-				--[[
-					
-					Appropriately adds value to this Vector2 object and returns the result.
-					
-				--]]
-				
-				if (type(value) == "number") then
-					return MODULE.addScalar(self, value)
-				elseif (type(value) == "table") then
-					if (value.className) and (value.className == "Vector2") then
-						return MODULE.addVectors(self, value)
-					else
-						error("attempt to perform arithmetic (add) on " .. self.className .. " and unknown")
-					end
-				else
-					error("attempt to perform arithmetic (add) on " .. self.className .. " and " .. type(value))
-				end
-				
-			end,
-			
-			__sub = function(self, value)
-									
-				--[[
-					
-					Appropriately subtracts value from this Vector2 objec and returns the resultt.
-					
-				--]]
-				
-				if (type(value) == "number") then
-					return MODULE.subtractScalar(self, value)
-				elseif (type(value) == "table") then
-					if (value.className) and (value.className == "Vector2") then
-						return MODULE.subtractVectors(self, value)
-					else
-						error("attempt to perform arithmetic (subtract) on " .. self.className .. " and unknown")
-					end
-				else
-					error("attempt to perform arithmetic (subtract) on " .. self.className .. " and " .. type(value))
-				end
-				
-			end,
-			
-			__mul = function(self, value)
-									
-				--[[
-					
-					Appropriately multiplies Vector2 object with value and returns the result.
-					
-				--]]
-				
-				if (type(value) == "number") then
-					return MODULE.multiplyScalar(self, value)
-				elseif (type(value) == "table") then
-					if (value.className) and (value.className == "Vector2") then
-						return MODULE.multiplyVectors(self, value)
-					else
-						error("attempt to perform arithmetic (multiply) on " .. self.className .. " and unknown")
-					end
-				else
-					error("attempt to perform arithmetic (multiply) on " .. self.className .. " and " .. type(value))
-				end
-				
-			end,
-			
-			__div = function(self, value)
-									
-				--[[
-					
-					Appropriately divides Vector2 object with value and returns the result.
-					
-				--]]
-				
-				if (type(value) == "number") then
-					return MODULE.divideScalar(self, value)
-				elseif (type(value) == "table") then
-					if (value.className) and (value.className == "Vector2") then
-						return MODULE.divideVectors(self, value)
-					else
-						error("attempt to perform arithmetic (divide) on " .. self.className .. " and unknown")
-					end
-				else
-					error("attempt to perform arithmetic (divide) on " .. self.className .. " and " .. type(value))
-				end
-				
-			end,
-			
-			__eq = function(self, value)
-									
-				--[[
-					
-					Returns true if the two Vector2 objects are within an epsilon of 0.01.
-					
-				--]]
-				
-				return MODULE.equals(self, value, 0.01)
-				
-			end,
-			
-			__tostring = function(self)
-									
-				--[[
-					
-					Returns a string representing the Vector2 object.
-					
-				--]]
-				
-				return ("( " .. self.x .. ", " .. self.y .. ")")
-				
-			end
+			__unm = umn,
+			__add = add,
+			__sub = sub,
+			__mul = mul,
+			__div = div,
+			__eq = eq,
+			__tostring = tostringMetamethod
 		}
 	)
 	
@@ -276,151 +200,55 @@ end
 --// METHODS //--
 
 function MODULE.addVectors(firstVector2, secondVector2)
-	
-	--[[
-		
-		Adds two Vector2 objects.
-		
-	--]]
-	
 	return MODULE.new(firstVector2.x + secondVector2.x, firstVector2.y + secondVector2.y)
-	
 end
 
 function MODULE.subtractVectors(firstVector2, secondVector2)
-	
-	--[[
-		
-		Subtracts two Vector2 objects.
-		
-	--]]
-	
 	return MODULE.new(firstVector2.x - secondVector2.x, firstVector2.y - secondVector2.y)
-	
 end
 
 function MODULE.multiplyVectors(firstVector2, secondVector2)
-	
-	--[[
-		
-		Multiplies two Vector2 objects.
-		
-	--]]
-	
 	return MODULE.new(firstVector2.x * secondVector2.x, firstVector2.y * secondVector2.y)
-	
 end
 
 function MODULE.divideVectors(firstVector2, secondVector2)
-	
-	--[[
-		
-		Divides two Vector2 objects.
-		
-	--]]
-	
 	return MODULE.new(firstVector2.x / secondVector2.x, firstVector2.y / secondVector2.y)
-	
 end
 
 function MODULE.addScalar(vector2, scalar)
-	
-	--[[
-		
-		Adds a scalar to Vector2 object.
-		
-	--]]
-	
 	return MODULE.new(vector2.x + scalar, vector2.y + scalar)
-	
 end
 
 function MODULE.subtractScalar(vector2, scalar)
-	
-	--[[
-		
-		Subtracts a scalar from Vector2 object.
-		
-	--]]
-	
 	return MODULE.new(vector2.x - scalar, vector2.y - scalar)
-	
 end
 
 function MODULE.multiplyScalar(vector2, scalar)
-	
-	--[[
-		
-		Multiplies a Vector2 object with a scalar.
-		
-	--]]
-	
 	return MODULE.new(vector2.x * scalar, vector2.y * scalar)
-	
 end
 
 function MODULE.divideScalar(vector2, scalar)
-	
-	--[[
-		
-		Divides a Vector2 object with a scalar.
-		
-	--]]
-	
 	return MODULE.new(vector2.x / scalar, vector2.y / scalar)
-	
 end
 
 function MODULE.mapVector(vector2, mapFunction)
-	
-	--[[
-		
-		Maps the values of a Vector2 object with a mapping function.
-		
-	--]]
-	
 	local x, y = mapFunction(vector2.x, vector2.y)
 	return MODULE.new(x, y)
-	
 end
 
 function MODULE.equals(firstVector2, secondVector2, epsilon)
-	
-	--[[
-		
-		Returns true if the two Vector2 objects are within epsilon difference.
-		
-	--]]
-	
 	return ((firstVector2 - secondVector2).magnitude < epsilon)
-	
 end
 
 function MODULE.getMagnitude(vector2)
-		
-	--[[
-		
-		Returns the magnitude of a Vector2 object.
-		
-	--]]
-	
 	return math.sqrt(
 		vector2.x^2 +
 		vector2.y^2
 	)
-	
 end
 
 function MODULE.getUnitVector(vector2)
-		
-	--[[
-		
-		Returns a unit Vector2 object for some Vector2.
-		
-	--]]
-	
 	return (vector2/vector2.magnitude)
-	
 end
 
 --// INSTRUCTIONS //--
